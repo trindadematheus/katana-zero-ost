ff7b1051682a25340f0ac7c3ec8b7ae552737f1f
Matheus Trindade <trindadematheus27@gmail.com>
2022-05-31 13:19:28 -0300

    base project

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { Router, Route } from "svelte-navigator";
+
+  import Menu from "./pages/Menu.svelte";
+  import Player from "./pages/Player.svelte";
+
+  import "./styles/global.css";
+</script>
+
+<Router>
+  <Route path="/">
+    <Menu />
+  </Route>
+
+  <Route path="player">
+    <Player />
+  </Route>
+</Router>
---
+++ src/pages/Menu.svelte
@@ -0,0 +1,17 @@
+<div class="">
+  <h3>login teste</h3>
+</div>
+
+<style>
+  div {
+    background-color: blue;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    height: 100vh;
+  }
+  h3 {
+    color: #eee;
+  }
+</style>
